9c08a44d3ad7690fdb6e8d5301452f928ac74503
Nileshpatidar177 <patidarnilesh163@gmail.com>
2026-04-12 21:37:26 +0530

    2 3

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Card = () => {
+  return <div className="card">
+    <h1>Nilesh Patidar</h1>
+    <p>Basically i am from mandsour but currently live indore becouse of education ,i am parsuing B.Tech computer Science Svvv Indore </p>
+  </div>
+}
+
+export default Card
